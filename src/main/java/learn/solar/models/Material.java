d8dc0f06e79221290ee97398811c05f195d518b2
darienmiller88 <darienmiller88@yahoo.com>
2024-07-28 02:23:19 -0400

    completed models

--- src/main/java/learn/solar/models/Material.java
+++ src/main/java/learn/solar/models/Material.java
@@ -1,5 +1,20 @@
 package main.java.learn.solar.models;
 
 public enum Material {
-    
+    McSi("Multicrystalline Silicon"),
+    MonoSi("Monocrystalline Silicon"),
+    ASi("Amorphous Silicon"),
+    CdTe("Cadmium Telluride"),
+    CIGS("Copper Indium Gallium Selenide");
+
+    Material(String materialName){
+        this.materialName = materialName;
+    }
+
+    @Override
+    public String toString(){
+        return materialName;
+    }
+
+    private String materialName;
 }
